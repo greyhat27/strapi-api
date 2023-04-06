7f20d79568acbefff980d02c9802f978bb7d4fcc
Abhishek Katore <katoreabhi27@gmail.com>
2023-04-06 23:06:31 +0530

    made changes in order.js

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,64 @@
-'use strict';
+// 'use strict';
+
+// /**
+//  * order controller
+//  */
+
+// const { createCoreController } = require('@strapi/strapi').factories;
+
+// module.exports = createCoreController('api::order.order');
+
+("use strict");
+
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(cxt) {
+    const { products } = cxt.request.body;
+    const lineItems = await Promise.all(
+      products.map(async (product) => {
+        const item = await strapi
+          .service("api::product.product")
+          .findOne(product.id);
+          return{
+            price_data:{
+                currency:"inr",
+                product_data:{
+                    name: item.title
+                },
+                unit_amount: item.price*100
+            },
+            quantity: product.quantity,
+          }
+      })
+    );
+    try {
+      const session = await stripe.checkout.sessions.create({
+        mode: "payment",
+        success_url: `${process.env.CLIENT_URL}?success=true`,
+        cancel_url: `${process.env.CLIENT_URL}?success=false`,
+        line_items: lineItems,
+        shipping_address_collection:{allowed_countries:["IN", "US"]},
+        payment_method_types:["card"]
+      });
+      await strapi.service("api::order.order").create({data:{
+        products,
+        stripeId: session.id,
+      }})
+      return {stripeSession: session}
+    } catch (err) {
+        console.log(err)
+      cxt.response.status = 500;
+      return err;
+    }
+  },
+}));
+
+
